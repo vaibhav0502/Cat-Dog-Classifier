61e49f9e9c5f8479495dd42202a0ad8a7d870f7f
Vaibhav Vasant Kumbhar <42543380+vaibhav0502@users.noreply.github.com>
2019-01-26 23:49:06 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,47 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier =  Sequential()
+
+# convolutional
+classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3),activation='relu'))
+# Pooling
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# second Convolution layers
+classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flattening
+classifier.add(Flatten())
+
+#Full connection
+classifier.add(Dense(output_dim = 128, activation='relu'))
+classifier.add(Dense(output_dim = 1, activation='sigmoid'))
+
+#Compiling
+classifier.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+
+#Fitting cnn to images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1./255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',target_size=(64, 64),batch_size=32,class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',target_size=(64, 64),batch_size=32,class_mode='binary')
+
+classifier.fit_generator(training_set,steps_per_epoch=500,epochs=2,validation_data=test_set,nb_val_samples=2000)
+
+model_json = classifier.to_json()
+with open("./model.json","w") as json_file:
+    json_file.write(model_json)
+
+classifier.save('model.h5')
+
+
